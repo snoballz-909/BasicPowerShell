2a681d2f2cd0e7a5d6c350e46b76f60cb0dcd389
snoballz-909 <ryan.cleary.bife@gmail.com>
2025-01-03 16:12:01 +0000

    Update Working with the pipeline deeper.ps1

--- Working with the pipeline deeper.ps1
+++ Working with the pipeline deeper.ps1
@@ -3,3 +3,6 @@ Import-Csv .\aliases.txt | New-Alias
 
 # Importing a CSV of new users, parsing that CSV file via the pipeline and using the AD module to create those new users with the relevant properties
 Import-Csv .\newusers.csv | Select-Object -Property *, @{name='samAccountName';Expression={$_.login}}, @{label='Name';expression{$_.login}}, @{n='Department';e={$_.dept}} | New-ADUser
+
+# Retreive a list of commands from any locally installed module thats starts with the word "Microsoft"
+Get-Command -Module (Get-Module -ListAvailable -Name Microsoft.* | Select-Object -ExpandProperty name)
