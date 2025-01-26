ac0393469a5909aa07f2fb131baf2e6dcfc064c9
snoballz-909 <ryan.cleary.bife@gmail.com>
2025-01-26 16:24:53 +0000

    Update PS-Remoting.ps1

--- PS-Remoting.ps1
+++ PS-Remoting.ps1
@@ -23,3 +23,20 @@ Invoke-Command -ScriptBlock {Get-Process | Sort-Object VM -Descending | Select-O
 
 # Retrieve the newest files from the home directory from multiple remote hosts
 Invoke-Command -ScriptBlock {Get-ChildItem ~\* | Sort-Object -Property LastWriteTime -Descending | Select-Object -First 10} -ComputerName (Get-Content C:\Users\User1\hostnames.txt) -Credential Administrator
+
+# Create a session which is stored in memory
+New-PSSession -ComputerName Desktop1,Desktop2
+
+# Retrieve the running remote sessions
+Get-PSSession
+
+# Store sessions in a variable and enter that session
+$session1 = New-PSSession -ComputerName Desktop1,Desktop2
+Enter-PSSession -Session $session1
+Get-PSSession -ComputerName Desktop1 | Enter-PSSession
+
+# Run a command from a remote session
+Invoke-Command -Command {Get-Process} -Session $session1
+
+# Close all open sessions from your shell
+Get-PSSession | Remove-PSSession
